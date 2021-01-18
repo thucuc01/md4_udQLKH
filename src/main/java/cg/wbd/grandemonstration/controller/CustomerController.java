7d29543110d8430af9fa8ac0752ad470e278ca98
thucuc01 <70868459+thucuc01@users.noreply.github.com>
2021-01-18 09:59:53 +0700

    bt2

---
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -0,0 +1,30 @@
+package cg.wbd.grandemonstration.controller;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public ModelAndView showList() {
+
+        ModelAndView modelAndView = new ModelAndView("customers/list.jsp");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+
+}
